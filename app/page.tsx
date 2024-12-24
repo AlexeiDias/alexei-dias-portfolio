21ffba3007a9c555a352606c0c5cf12582cd7cad
Alexei Dias <alexeidias@Alexeis-MacBook-Pro.local>
2024-12-24 01:43:53 -0800

    project created and running

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-import Header from './components/Header'
-import Hero from './components/Hero'
-import Skills from './components/Skills'
-import Projects from './components/Projects'
-import Contact from './components/Contact'
-import Footer from './components/Footer'
-import ParallaxSection from './components/ParallaxSection'
+import Header from '../components/Header';
+import Hero from '../components/Hero';
+import Skills from '../components/Skills';
+import Projects from '../components/Projects';
+import Contact from '../components/Contact';
+import Footer from '../components/Footer';
+import ParallaxSection from '../components/ParallaxSection';
+
 
 export default function Home() {
   return (
